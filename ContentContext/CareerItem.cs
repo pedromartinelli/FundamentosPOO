f6c5542e23900c99aafc2daf72b66c5adec02c84
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-03-19 12:12:35 -0300

    regras de negocio

--- ContentContext/CareerItem.cs
+++ ContentContext/CareerItem.cs
@@ -4,6 +4,8 @@
     {
         public CareerItem(int order, string title, string description, Course course)
         {
+            if (course == null) throw new Exception("O curso não pode ser nulo");
+
             Order = order;
             Title = title;
             Description = description;

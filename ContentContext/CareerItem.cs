7e30332e0526fed6381fab180a6310f42c2b0d29
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-03-19 12:04:23 -0300

    interagindo com listas

--- ContentContext/CareerItem.cs
+++ ContentContext/CareerItem.cs
@@ -2,6 +2,14 @@
 {
     public class CareerItem
     {
+        public CareerItem(int order, string title, string description, Course course)
+        {
+            Order = order;
+            Title = title;
+            Description = description;
+            Course = course;
+        }
+
         public int Order { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
--- Program.cs
+++ Program.cs
@@ -11,11 +11,43 @@ namespace fundamentosPOO
             articles.Add(new Article("Aprenda sobre C#", "csharp"));
             articles.Add(new Article("Aprenda sobre EF", "entity-framework"));
 
-            foreach (var article in articles)
+            //foreach (var article in articles)
+            //{
+            //    Console.WriteLine(article.Id);
+            //    Console.WriteLine(article.Title);
+            //    Console.WriteLine(article.Url);
+            //}
+
+            var courses = new List<Course>();
+            var careers = new List<Career>();
+
+            var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+            var courseCsharp = new Course("Fundamentos C#", "fundamentos-csharp");
+            var courseEf = new Course("Fundamentos Entity Framework", "fundamentos-ef");
+            courses.Add(courseOOP);
+            courses.Add(courseCsharp);
+            courses.Add(courseEf);
+
+
+            var careerItem1 = new CareerItem(1, "Comece por aqui", "Aprenda C#", courseCsharp);
+            var careerItem2 = new CareerItem(2, "Avance no desenvolvimento", "Aprenda OOP", courseOOP);
+            var careerItem3 = new CareerItem(3, "Acesso a dados", "Aprenda EF", courseEf);
+
+            var careerDotnet = new Career("Especialista .NET", "especialista-dotnet");
+            careerDotnet.Items.Add(careerItem2);
+            careerDotnet.Items.Add(careerItem1);
+            careerDotnet.Items.Add(careerItem3);
+
+            careers.Add(careerDotnet);
+
+            foreach (var career in careers)
             {
-                Console.WriteLine(article.Id);
-                Console.WriteLine(article.Title);
-                Console.WriteLine(article.Url);
+                Console.WriteLine($"\n{career.Title}");
+                foreach (var careerItem in career.Items.OrderBy(x => x.Order))
+                {
+                    Console.WriteLine($"{careerItem.Order} - {careerItem.Title}");
+                }
+                Console.WriteLine($"\n");
             }
         }
     }
